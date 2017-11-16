53552805c02930d0ee92ce3766059f4c995ba72a
Benjamin Sullins <bensullins1@gmail.com>
2017-11-15 21:45:14 -0700

    Fixed Image Scaling colormap to be data aligned with bypass mode. Added switch for colormap component

--- matlab/coe_generator/ANUPimage2coe.m
+++ matlab/coe_generator/ANUPimage2coe.m
@@ -1,11 +1,10 @@
-% Image to text conversion
-
+%% Image to text conversion
 
 % Read the image from the file
- [filename, pathname] = uigetfile('*.bmp;*.tif;*.jpg;*.pgm','Pick an M-file');
+[filename, pathname] = uigetfile('*.bmp;*.tif;*.jpg;*.pgm','Pick an M-file');
 img = imread(strcat(pathname, filename));
 img = imresize((img),[120 160]);
-[ row col p ] =size(img);
+[ row col p ] = size(img);
 
 if p == 3
     img = rgb2gray(img);
@@ -19,9 +18,8 @@ end
 
 % Image Transpose
 imgTrans = img';
-%imgTrans = img;
 
-% iD conversion
+% 1D conversion
 img1D = imgTrans(:);
 % Decimal to Hex value conversion
 imgHex = dec2hex(img1D);
@@ -32,4 +30,24 @@ fprintf(fid,'memory_initialization_radix=16;\n');
 fprintf(fid,'memory_initialization_vector=\n');
 fprintf(fid, '%x\n', img1D);
 % Close the txt file
-fclose(fid)
+fclose(fid);
+
+%% Create the colormapping
+numVidBits = 8;
+numVgaBits = 4;
+
+myColormap = colormap('pink');
+myRange    = cast(myColormap*(2^numVidBits), 'uint8') / (2^(numVidBits-numVgaBits));
+myRange    = myRange - 1; myRange(myRange < 0) = 0;
+myRangeHex = [ dec2hex(myRange(:,1)), dec2hex(myRange(:,2)), dec2hex(myRange(:,3))];
+myMapTemp  = myRangeHex(1:(numVidBits-numVgaBits):end,:);
+myMap      = myMapTemp';
+
+% New txt file creation
+fid = fopen('colorMapping.coe', 'wt');
+% Hex value write to the txt file
+fprintf(fid,'memory_initialization_radix=16;\n');
+fprintf(fid,'memory_initialization_vector=\n');
+fprintf(fid, '%c%c%c\n', myMap);
+% Close the txt file
+fclose(fid);
